f081b0eb88844456e3ee16ab1fcc8de2b1b44b61
Alexandre Cliche <a_cliche@msn.com>
2016-11-13 15:30:27 -0500

    v1.0 Completed Section

---
+++ TwinStick2.5D/Assets/Camera/CameraPan.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPan : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void LateUpdate () {
+		transform.LookAt (target);
+	}
+}
--- TwinStick2.5D/Assets/GAME/GameManager.cs
+++ TwinStick2.5D/Assets/GAME/GameManager.cs
@@ -6,10 +6,15 @@ public class GameManager : MonoBehaviour {
 
 	public bool recording = true;
 
+	private bool isPaused = false;
+	private float initialFixedDeltaTime;
+
 	void Start () {
 		PlayerPrefsManager.UnlockLevel (1);
 		print (PlayerPrefsManager.IsLevelUnlocked (1));
 		print (PlayerPrefsManager.IsLevelUnlocked (2));
+		initialFixedDeltaTime = Time.fixedDeltaTime;
+		print (initialFixedDeltaTime);
 	}
 
 	// Update is called once per frame
@@ -19,5 +24,22 @@ public class GameManager : MonoBehaviour {
 		} else {
 			recording = true;
 		}
+		if (Input.GetKeyDown (KeyCode.P)  && isPaused) {
+			isPaused = false;
+			ResumeGame ();
+		} else if (Input.GetKeyDown (KeyCode.P) && !isPaused) {
+			isPaused = true;		
+			PauseGame ();
+		}
+	}
+
+	void PauseGame () {
+		Time.timeScale = 0;
+		Time.fixedDeltaTime = 0;
+	}
+
+	void ResumeGame () {
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = initialFixedDeltaTime;
 	}
 }
